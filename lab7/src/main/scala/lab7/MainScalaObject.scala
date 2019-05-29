d7c0ee5915a9a160591399c7ceea4bfb32e792bb
EfimSirotkin <44836472+EfimSirotkin@users.noreply.github.com>
2019-05-29 23:48:46 +0300

    Add files via upload
    lab7 on Scala

---
+++ lab7/src/main/scala/lab7/MainScalaObject.scala
@@ -0,0 +1,71 @@
+package lab7
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+
+object MainScalaObject {
+  def main(args: Array[String]): Unit ={
+    println("Hello world!")
+    val list = List[Int](1,2,3,4,5,6,7,8)
+    val list1 = mutable.MutableList[Int](1, 2, 0, 0, -3)
+    val list2 = mutable.MutableList[Int]()
+    println("Recursive sum: " +sumRecursive(list,list.length))
+    println("Tail recursion sum: " + sumTailRecursive(list))
+    println( list1 + " <2 previous: " + bigNumNotRec(list1, list2) + "[no_tail]")
+    println(list1 + " <2 previous: " + bigNumTail(list1) + "[tail]")
+
+  }
+
+  def sumRecursive(list: List[Int],size: Int): Int = {
+    if(size > 0) list(size - 1) + sumRecursive(list, size - 1)
+    else 0
+  }
+
+  def sumTailRecursive(list: List[Int]): Int = {
+    @tailrec
+    def sumIter(list: List[Int], acc:Int, index:Int):Int={
+      if(index < list.length) sumIter(list, list(index) + acc, index+1)
+      else acc
+    }
+    sumIter(list, 0, 0)
+  }
+
+  def bigNumNotRec(oldList: mutable.MutableList[Int], newList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
+    for(i <- 2 to oldList.size - 1){
+      if(oldList(i) < Math.abs(oldList(i - 1) - oldList(i - 2))) newList += oldList(i)
+    }
+    newList
+  }
+
+  def sumList(list: mutable.MutableList[Int]): mutable.MutableList[Int] = {
+    val sum = list.reduceLeft(_ + _)
+    list.filter((i:Int) => i % 1 == 0)
+    list += sum
+    list
+  }
+
+  def bigNumTail(oldList: mutable.MutableList[Int]): mutable.MutableList[Int] = {
+    @tailrec
+    def find(oldList: mutable.MutableList[Int], newList: mutable.MutableList[Int], index: Int): mutable.MutableList[Int] = {
+      if (index < oldList.size) {
+        isValueTrueAdd(newList, oldList(index - 1), oldList(index), oldList(index - 2))
+        find(oldList, newList, index + 1)
+      }
+      else {
+        newList
+      }
+    }
+    def isValueTrueAdd(newList: mutable.MutableList[Int], valuePrev: Int, value: Int, valuePrevPrev: Int): Unit = {
+      if (Math.abs(valuePrev - valuePrevPrev) > value) {
+        newList += value
+      }
+    }
+    if (oldList.size > 2) {
+      find(oldList, mutable.MutableList(), 2)
+    }
+    else {
+      mutable.MutableList()
+    }
+  }
+}
